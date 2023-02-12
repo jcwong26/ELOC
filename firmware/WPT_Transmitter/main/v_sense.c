c743eaf3dee20f8b2d2d6f24f0b954d14dfe3868
Joseph Borromeo <jaborrom@uwaterloo.ca>
2023-02-12 09:43:56 -0500

    starting on ADC things

--- firmware/WPT_Transmitter/main/v_sense.c
+++ firmware/WPT_Transmitter/main/v_sense.c
@@ -1,2 +1,29 @@
 #include "v_sense.h"
 
+#include "sdkconfig.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_log.h"
+#include "esp_err.h"
+#include "esp_adc/adc_continuous.h"
+
+
+void init_adcs(void){
+    adc_continuous_handle_t handle = NULL;
+    
+    adc_continuous_handle_cfg_t adc_config = {
+        .max_store_buf_size = 1024,
+        .conv_frame_size = 100,
+    };
+    ESP_ERROR_CHECK(adc_continuous_new_handle(&adc_config, &handle));
+}
+
+void current_monitoring_task(void*){
+    // Setup queue or something here
+    printf("test");
+}
+
+void supply_voltage_monitor_task(void*){
+    // Setup Queue or something here
+    printf("test");
+}
--- firmware/WPT_Transmitter/main/v_sense.h
+++ firmware/WPT_Transmitter/main/v_sense.h
@@ -2,5 +2,15 @@
 #define CURR_SENSE_PIN 5
 #define V_INV_SENSE_PIN 6
 
-#define V_DIV_RATIO 0.0507614
+#define V_PER_V_INV 20
+#define R_SHUNT_OHMS 0.01
+
+#define V_24V_DIV_RATIO 0.0507614
 #define MAX_ADC_OUTPUT 4095
+
+#define V_TO_I_SHUNT(volts) ((volts/V_PER_V_INV)/R_SHUNT_OHMS)
+
+void init_adcs(void);
+
+void current_monitoring_task(void*);
+void supply_voltage_monitor_task(void*);
